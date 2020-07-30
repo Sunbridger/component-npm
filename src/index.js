d71863b0142e0a1d2c291e32dfe8714e1abd6024
Sunbridger <739272884@qq.com>
2020-07-30 17:05:19 +0800

    feat: upgrade

--- src/index.js
+++ src/index.js
@@ -1,44 +1,43 @@
-var request = require('request'); 
-const cheerio = require('cheerio')
+var request = require("request");
+const cheerio = require("cheerio");
 const baseUrl = "https://www.pearvideo.com/popular";
 const url_to = "https://www.pearvideo.com/";
-let arr = [] ; //暂时用全局的变量承接；
+let arr = []; //暂时用全局的变量承接；
 function startReq(url = baseUrl) {
-  let newUrlArr = [];
-//   let arr=[];
-  let type = [10,1,2,9,5,8,4,3,31,6,59];
-  let ranIndex=Math.random()*10|0;
-  url = baseUrl+'_'+type[ranIndex];
-  request(url,  function (error, response, body) {
-      const $ = cheerio.load(body); 
-      const data = $('.popularembd');
-      data.map(el=>{
-        newUrlArr.push(url_to+data[el].attribs.href);
-      })
-      newUrlArr=[...new Set(newUrlArr)];
-      newUrlArr.map(newurl=>{
-        request(newurl,(err,res,body)=>{
-            const $=cheerio.load(body);
-            const videoURL = $("script").eq(6).html().split(';')[0].split("srcUrl=")[1].split(",vdoUrl")[0].trim()
-            arr.push(JSON.parse(videoURL))
-        }) 
-      })
-  });
+    let newUrlArr = [];
+    //   let arr=[];
+    let type = [10, 1, 2, 9, 5, 8, 4, 3, 31, 6, 59];
+    let ranIndex = (Math.random() * 10) | 0;
+    url = baseUrl + "_" + type[ranIndex];
+    request(url, function (error, response, body) {
+        const $ = cheerio.load(body);
+        const data = $(".popularembd");
+        data.map((el) => {
+            newUrlArr.push(url_to + data[el].attribs.href);
+        });
+        newUrlArr = [...new Set(newUrlArr)];
+        newUrlArr.map((newurl) => {
+            request(newurl, (err, res, body) => {
+                const videoURL= cheerio.load(body).html().split('srcUrl=')[1].split(',vdoUrl=srcUrl')[0];
+                arr.push(videoURL);
+            });
+        });
+    });
 }
 startReq();
-module.exports=new Promise(function(resolve,reject){
-    setTimeout(()=>{
-        if(arr.length==0){
+module.exports = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        if (arr.length == 0) {
             reject({
-                msg:'网络缓慢，请重试',
-                status:-1,
-                data: []
-            })
-        }else{
+                msg: "网络缓慢，请重试",
+                status: -1,
+                data: [],
+            });
+        } else {
             resolve({
-                status:1,
-                data:arr
+                status: 1,
+                data: arr,
             });
         }
-    },1000)
-})
+    }, 2000);
+});
